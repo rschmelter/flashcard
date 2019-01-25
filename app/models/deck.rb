ebd7f38b610d9113c5425818c5d707852d8239c7
rschmelter <rschmelter33@gmail.com>
2019-01-25 12:10:10 -0800

    add associations to User, Deck, Card

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,2 +1,7 @@
 class Deck < ApplicationRecord
+
+    belongs_to :user
+    has_many :cards
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :decks
+
+    validates :name, uniqueness: true
+
 end
